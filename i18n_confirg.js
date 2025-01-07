2d29b73bfc864e33a60be984161f1ced252c36c1
Javier Rodriguez <franciscoromera08162gmail.com>
2025-01-07 20:22:41 +0100

    Fix i18n config

--- i18n_confirg.js
+++ i18n_confirg.js
@@ -1,11 +1,14 @@
-const i18n = require('i18n');
-const path = require('path');
+import { I18n } from 'i18n'
+import path from 'node:path'
+import { __dirname } from './utils.js'
 
-i18n.configure({
-    locales: ['es', 'en'],
-    directory: path.join(__dirname, 'locales'),
-    defaultLocale: 'es',
-    cookie: 'lang'
-});
+const i18n = new I18n({
+  locales: ['en', 'es'],
+  directory: path.join(__dirname, '..', 'locales'),
+  defaultLocale: 'en',
+  autoReload: true, // watch for changes in JSON files to reload locale on updates - defaults to false
+  syncFiles: true, // sync locale information across all files - defaults to false
+  cookie: 'nodeapp-locale',
+})
 
-module.exports = i18n;
+export default i18n
