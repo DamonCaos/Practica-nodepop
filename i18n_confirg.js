453e6d56233e117c206ae19dda77b3b5ce72b05e
Javier Rodriguez <franciscoromera08162gmail.com>
2025-01-07 20:19:56 +0100

    start node practice, internacionalizacion

---
+++ i18n_confirg.js
@@ -0,0 +1,11 @@
+const i18n = require('i18n');
+const path = require('path');
+
+i18n.configure({
+    locales: ['es', 'en'],
+    directory: path.join(__dirname, 'locales'),
+    defaultLocale: 'es',
+    cookie: 'lang'
+});
+
+module.exports = i18n;
